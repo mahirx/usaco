760afffb0a49ed7fbe0a1682f9ce96bb87649611
mahirx <60250437+mahirx@users.noreply.github.com>
2021-05-22 20:34:27 +0600

    created

--- General/Basketball_one_on_one.cpp
+++ General/Basketball_one_on_one.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
+#include <stdio.h>
 
 int main() {
 	string s; cin >> s;
 	cout << s[s.size()-2] << endl;
 	cout << s.size();
+	printf("\n");
 	// cout << s;
+	cout << s
 }
+
